8a82a3295bbbfd124937588b38b25e7fb065fa80
iamoki <76725512+iamoki@users.noreply.github.com>
2022-01-20 01:17:40 +0900

    리액트 첫세팅

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+function App() {
+  return (
+    <Fragment>
+      <h1>리액트 안녕!</h1>
+      <h2>잘 작동하니????</h2>
+      <h3>화이팅팅팅</h3>
+      
+    </Fragment>
+  );
+}
+
+export default App;
