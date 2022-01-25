6200c1536d65b0cb2aa4a2c470db700d7733bd5b
iamoki <76725512+iamoki@users.noreply.github.com>
2022-01-25 23:02:17 +0900

    리액트를 다루는기술 4장 - 4.2예제로 이벤트 핸들링 익히기(onChange, onKeypress...)

---
+++ src/EventPractice.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+
+class EventPractice extends Component {
+    
+    state = {
+        username: '',
+        message: '',
+    }
+
+    handleChange = (e) => { // transform-class-properties 문법
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    handleClick = () => {
+        alert(this.state.username + ': ' + this.state.message);
+        this.setState({
+            username: '',
+            message: '',
+        });
+    }
+
+    handleKeyPress = (e) => {
+        if(e.key === "Enter") {
+            this.handleClick();
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>이벤트 연습</h1>
+                <input
+                    type="text"
+                    name="username"
+                    placeholder="사용자명"
+                    value={this.state.username}
+                    onChange={this.handleChange}
+                />
+                <input
+                    type="text"
+                    name="message"
+                    placeholder="아무거나 입력해 보세요"
+                    value={this.state.message}
+                    onChange = {this.handleChange}
+                    onKeyPress={this.handleKeyPress}
+                />
+                <button onClick={this.handleClick}>확인</button>
+            </div>
+        );
+    }
+}
+
+export default EventPractice;
+
+
+// render() {
+//     return (
+//         <div>
+//             <h1>이벤트 연습</h1>
+//             <input
+//                 type="text"
+//                 name="message"
+//                 placeholder="아무거나 입력해 보세요"
+//                 value={this.state.message}
+//                 onChange = { //컴포넌트에 직접 이벤트핸들링
+//                     (e) => {
+//                         this.setState({
+//                             message: e.target.value
+//                         })
+//                     }
+//                 }
+//             />
+//             <button onClick={ //컴포넌트에 직접 이벤트핸들링
+//                 () => {
+//                     alert(this.state.message);
+//                     this.setState({
+//                         message: ''
+//                     });
+//                 }
+//             }>확인
+//             </button>
+//         </div>
+//     );
+// }
+
+    // 콘스트럭처활용 
+    // constructor(props) {
+    //     super(props);
+    //     this.handleChange = this.handleChange.bind(this);
+    //     this.handleClick = this.handleClick.bind(this);
+    // }
+
+    // handleChange(e) {
+    //     this.setState({
+    //         message: e.target.value
+    //     });
+    // }
+
+    // handleClick(e) {
+    //     this.setState({
+    //         message: ''
+    //     })
+    // }
+    
