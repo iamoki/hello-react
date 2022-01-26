cb793a6009656e546a40028e7753c2b705d5893b
iamoki <76725512+iamoki@users.noreply.github.com>
2022-01-26 22:56:57 +0900

    태그에 ref달아보기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import EventPractice from './EventPractice';
+import React, {Component} from 'react';
+import ValidationSample from './ValidationSample'
 
-const App = () => {
-  return <EventPractice />;
-};
+class App extends Component {
+  render() {
+    return (
+      <ValidationSample/>
+    );
+  }
+}
 
 export default App;
 
