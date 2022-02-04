adb5ac8dc98fb1189994223a99df66ae15e8412d
iamoki <76725512+iamoki@users.noreply.github.com>
2022-02-04 17:19:45 +0900

    scss 추가/적용

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import LifeCycleSample from './LifeCycleSample';
 import ErrorBoundary from './ErrorBoundary';
+import './SassComponent.scss'
 
 function getRandomColor() {
   return '#' + Math.floor(Math.random() * 16777215).toString(16);
@@ -20,7 +21,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <button onClick={this.handleClick}>랜덤 색상</button>
+        <button className="btn blue" onClick={this.handleClick}>랜덤 색상</button>
         <ErrorBoundary>
           <LifeCycleSample color={this.state.color}/>
         </ErrorBoundary>
--- src/LifeCycleSample.js
+++ src/LifeCycleSample.js
@@ -69,12 +69,12 @@ class LifeCycleSample extends Component {
 
         return (
             <div>
-                {this.props.missing.value}
+                {/* {this.props.missing.value} */}
                 <h1 style={style} ref={ref => this.myRef=ref}>
                     {this.state.number}
                 </h1>
                 <p>color: {this.state.color}</p>
-                <button onClick={this.handleClick}>
+                <button className="btn orange" onClick={this.handleClick}>
                     더하기
                 </button>
             </div>
